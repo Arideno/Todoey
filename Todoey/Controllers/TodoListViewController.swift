6106b5b3f7d5c8dcfb8afe87146a21cf6e47e22a
Arideno <andrii.moisol@gmail.com>
2018-01-10 18:46:28 +0200

    Persisting data using Codable protocol and Plist.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -12,26 +12,12 @@ class TodoListViewController: UITableViewController {
 	
 	var itemArray = [Item]()
 	
-	let defaults = UserDefaults.standard
+	let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
 		
-		let newItem = Item()
-		newItem.title = "Find Mike"
-		itemArray.append(newItem)
-		
-		let newItem2 = Item()
-		newItem2.title = "Buy Eggos"
-		itemArray.append(newItem2)
-		
-		let newItem3 = Item()
-		newItem3.title = "Destroy Demogorgon"
-		itemArray.append(newItem3)
-		
-		if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-			itemArray = items
-		}
+		loadItems()
 
     }
 
@@ -59,6 +45,8 @@ class TodoListViewController: UITableViewController {
 		
 		itemArray[indexPath.row].done = !itemArray[indexPath.row].done
 		
+		saveItems()
+		
 		tableView.reloadData()
 		
 		tableView.deselectRow(at: indexPath, animated: true)
@@ -79,9 +67,7 @@ class TodoListViewController: UITableViewController {
 			
 			self.itemArray.append(newItem)
 			
-			self.defaults.set(self.itemArray, forKey: "TodoListArray")
-			
-			self.tableView.reloadData()
+			self.saveItems()
 		}
 		
 		alert.addTextField { (alertTextField) in
@@ -93,6 +79,32 @@ class TodoListViewController: UITableViewController {
 		
 		present(alert, animated: true, completion: nil)
 		
+	}
+	
+	//MARK: - Model Manupulation Methods
+	
+	func saveItems() {
+		let encoder = PropertyListEncoder()
+		
+		do {
+			let data = try encoder.encode(itemArray)
+			try data.write(to: dataFilePath!)
+		} catch {
+			print("Error encoding item array, \(error)")
+		}
+		
+		self.tableView.reloadData()
+	}
+	
+	func loadItems() {
+		if let data = try? Data(contentsOf: dataFilePath!) {
+			let decoder = PropertyListDecoder()
+			do {
+				itemArray = try decoder.decode([Item].self, from: data)
+			} catch {
+				print("Error decoding item array, \(error)")
+			}
+		}
 	}
 	
 }
